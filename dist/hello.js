eddfd0a395c87132afa43b1b53fd7e79d03d5ba9
mkhaque1 <mkhaque29@gmail.com>
2026-06-07 14:28:26 +0200

    typescript setup and basic types

---
+++ dist/hello.js
@@ -0,0 +1,8 @@
+let name = 'karim'; // type annotation
+let number = 25; // type annotation
+let isActive = true;
+// let city: string = 25; // Type 'number' is not assignable to type 'string'. // wrong example
+let city = 'Dhaka'; // Type Inference
+let score = 20; // Type Inference
+export {};
+//# sourceMappingURL=hello.js.map
---
+++ src/hello.ts
@@ -0,0 +1,8 @@
+let name: string = 'karim'; // type annotation
+let number: number = 25; // type annotation
+let isActive: boolean = true;
+
+// let city: string = 25; // Type 'number' is not assignable to type 'string'. // wrong example
+
+let city = 'Dhaka'; // Type Inference
+let score = 20; // Type Inference
